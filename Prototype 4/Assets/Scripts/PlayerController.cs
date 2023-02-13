98654bffc4d7b2ef9eb187244c43ab017df38685
Giovani-YT <181396@student.vansd.org>
2023-02-13 10:19:19 -0800

    Personal Project
    Unit 3

--- Prototype 4/Assets/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -22,12 +22,14 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
         private void OnTriggerEnter(Collider other)
         {
             if(other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
         }
@@ -35,6 +37,7 @@ public class PlayerController : MonoBehaviour
             {
                 yield return new WaitForSeconds(7);
                 hasPowerup = false;
+                powerupIndicator.gameObject.SetActive(false);
             }
         }
         private void OnCollisionEnter(Collision collision)
